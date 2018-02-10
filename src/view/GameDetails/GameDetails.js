969eb09135209f6f0aab9dfeb215d892374bc1d2
Jason <jsnmgpnty@gmail.com>
2018-02-10 13:36:12 +0800

    added game details fetchinf of game

--- src/view/GameDetails/GameDetails.js
+++ src/view/GameDetails/GameDetails.js
@@ -10,7 +10,7 @@ import { TeamSelector, Spinner } from 'components';
 import { setCurrentPage, setGame, isPageLoading } from 'actions/navigation';
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentPage: () => dispatch(setCurrentPage(pageNames.gamePrepare)),
+  setCurrentPage: () => dispatch(setCurrentPage(pageNames.gameDetails)),
   isPageLoading: (isLoading) => dispatch(isPageLoading(isLoading)),
   setGame: (game) => dispatch(setGame(game)),
 });
@@ -21,9 +21,56 @@ const mapStateToProps = state => ({
 });
 
 class GameDetails extends PureComponent {
-  static propTypes = {
+  state = {
+    gameId: null,
+    isBusy: false,
+    isGameStarted: false,
+    isGameCompleted: false,
+    game: null,
+  };
+  
+  async componentDidMount() {
+    const { setCurrentPage } = this.props;
+    const { params } = this.props.match;
 
+    setCurrentPage();
+    this.setState({ gameId: params.gameId });
+    await this.getGame(params.gameId);
   }
+
+  getGame = async (gameId) => {
+    const { isBusy } = this.state;
+    if (isBusy) {
+      return;
+    }
+
+    this.setState({ isBusy: true });
+
+    try {
+      const game = await gameApi.getGame(gameId);
+
+      if (game) {
+        switch (game.status) {
+          case 'INPROGRESS':
+          case 'PAUSED':
+            this.setState({ isGameStarted: true });
+            break;
+          case 'COMPLETED':
+            this.setState({ isGameCompleted: true });
+            break;
+          default:
+            break;
+        }
+        
+        this.props.setGame(game);
+        this.setState({ game, isBusy: false }, this.isGameReady);
+      }
+    } catch (error) {
+      this.setState({ isBusy: false });
+      console.log(error);
+    }
+  };
+
 
   render() {
     return (
